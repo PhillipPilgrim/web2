7ceb2af6aa91858e87f8c05f26b941d26529ba1f
Phillip Pilgrim <filipmusalek.osobni@gmail.com>
2025-03-01 19:09:43 +0100

    Gix

--- src/app/api/submitContact/route.ts
+++ src/app/api/submitContact/route.ts
@@ -43,7 +43,6 @@ export async function GET(req: NextRequest) {
 	if (!token) return new Response(null, { status: 400 });
 
 	const contactFormData = new FormData();
-	// @ts-expect-error: If there is no expect error, there will be a type error in the runtime
 	const ip = req?.ip;
 
 	contactFormData.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY as string);
